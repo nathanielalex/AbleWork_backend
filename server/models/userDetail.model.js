ff7033791ee1be3d929fe57f657cfd7ad994e949
nathanielalex <nathanalex019@gmail.com>
2025-03-30 15:58:10 +0700

    Create userDetail.model.js

---
+++ server/models/userDetail.model.js
@@ -0,0 +1,21 @@
+import mongoose from "mongoose";
+
+const userDetailSchema = new mongoose.Schema({
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true,
+    ref: 'User' 
+  },
+  skill: {
+    type: [String], 
+    required: true
+  },
+  CV: {
+    type: String, //URL
+    required: true
+  }
+});
+
+const UserDetail = mongoose.model('UserDetail', userDetailSchema);
+
+export default UserDetail;
